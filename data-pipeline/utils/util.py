3002c25e6b28176d2abc39e7da0ef7f04b80f022
Aravind <aravind19112003@gmail.com>
2025-01-03 23:07:27 +0530

    Add basic working functionalities

--- data-pipeline/utils/util.py
+++ data-pipeline/utils/util.py
@@ -1,9 +1,39 @@
+import os
+import pandas as pd
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+connection_string = os.getenv("MONGO_CONN_STRING")
+client = MongoClient(connection_string)
+db = client['fake_account_data'] 
+
+real_users_df = None
+fake_users_df = None
 
 def extract_data_from_csv():
-    pass
+
+    global real_users_df, fake_users_df
+    load_dotenv()
+
+    real_users = 'data/users.csv' 
+    fake_users = 'data/fake_users.csv'
+
+    real_users_df = pd.read_csv(real_users)
+    fake_users_df = pd.read_csv(fake_users)
 
 def transform_data():
     pass
 
 def load_data_to_mongodb():
-    pass
+
+    real_users_collection = db['real_users']
+    fake_users_collection = db['fake_users']
+    
+    real_users_collection.drop()
+    fake_users_collection.drop()
+
+    real_users_collection.insert_many(real_users_df.to_dict('records'))
+    fake_users_collection.insert_many(fake_users_df.to_dict('records'))
+
+    print("Data loaded successfully!")
+
