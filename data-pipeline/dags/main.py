0dfea6146f836269ff83443beb8ccea1d960a6e2
Aravind <aravind19112003@gmail.com>
2025-01-03 21:46:11 +0530

    Add base

---
+++ data-pipeline/dags/main.py
@@ -0,0 +1,51 @@
+from airflow import DAG
+from airflow.decorators import task
+from airflow.utils.dates import days_ago
+from utils.util import extract_data_from_csv, transform_data, load_data_to_mongodb
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler('logs/main.log'),
+        logging.StreamHandler()
+    ]
+)
+
+logger = logging.getLogger(__name__)
+logger.info("Starting dag...")
+
+default_args={
+    'owner':'airflow',
+    'start_date':days_ago(1)
+}
+
+with DAG(dag_id='etl_pipeline',
+         default_args=default_args,
+         schedule_interval='@daily',
+         catchup=False) as dags:
+    
+    @task
+    def extract():
+       try:
+           extract_data_from_csv()
+       except:
+           pass
+
+    @task
+    def transform():
+        try:
+           transform_data()
+        except:
+           pass
+
+
+    @task
+    def load():
+        try:
+           load_data_to_mongodb()
+        except:
+           pass
+
+    extract() >> transform() >> load()
---
+++ data-pipeline/utils/util.py
@@ -0,0 +1,9 @@
+
+def extract_data_from_csv():
+    pass
+
+def transform_data():
+    pass
+
+def load_data_to_mongodb():
+    pass
